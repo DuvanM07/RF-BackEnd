64eb2fbf388539df578779227b610365f2800037
Juan Carlos Jiménez Gutiérrez <jangojes.dev@gmail.com>
2025-01-29 20:45:50 -0500

    [+] index.html: Define EndPoints base

--- src/index.js
+++ src/index.js
@@ -2,7 +2,26 @@ const express = require( 'express' );
 const app = express();
 
 
-/** Lanzar servidor */
+/** Endpoints del sitio */
+app.get( '/', function( req, res ) {
+    res.send( '<h1>Home</h1>' );
+});
+app.get( '/about-us', function( req, res ) {
+    res.send( '<h1>About us</h1>' );
+});
+app.get( '/contact', function( req, res ) {
+    res.send( '<h1>Contact</h1>' );
+});
+app.get( '/api', function( req, res ) {
+    res.json({
+        name: 'Restaurant Finder',
+        msg: 'Bienvenidos a Restaurant Finder'
+    });
+});
+
+
+
+/** Lanzar servidor: http://localhost:<port> */
 app.listen( 3000, function() {
     console.log( 'Servidor escuchando en el puerto 3000' );
 });
