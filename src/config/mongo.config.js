9d1bfafcd6aa091b4653974d323a775593492fcd
Juan Carlos Jiménez Gutiérrez <jangojes.dev@gmail.com>
2025-01-31 17:40:04 -0500

    [+] mongo.config.js: Configuracion de Mongo usando Mongoose [+] index.js: Invoca conexion a DB

---
+++ src/config/mongo.config.js
@@ -0,0 +1,33 @@
+const mongoose = require( 'mongoose' );
+
+/** Configuracion para conectar MongoDB usando la dependencia MongooseJS usando Async/Await */
+async function dbConection() {
+    // try-catch: Se usa para manejar excepciones
+    try {
+        await mongoose.connect( 'mongodb://localhost:27017/db-r-finder', {} );      // Conectamos a la base de datos y retorna promesa
+        console.log( 'Base de datos inicializada correctamente' );
+    } 
+    catch ( error ) {
+        console.error( error );
+        console.log( 'Ha ocurrido un error en la conexion de la BD' );
+        // throw new Error( 'Error al inicializar la base datos' );
+    }
+    
+}
+
+
+/** Configuracion desde la pagina oficial de MongooseJS usando then-catch */
+function conexionDB () {
+    mongoose.connect( 'mongodb://localhost:27017/db-r-finder', {})
+        .then( function( data ) {
+            console.log( 'Base de datos conectada exitosamente' );
+        } )
+        .catch( function( error ) {
+            console.error( error );
+            console.log( 'Ha ocurrido un error en la conexion de la BD' );
+        } );
+}
+
+
+//module.exports = dbConection;       // Exportando la configuracion
+module.exports = conexionDB;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 const express = require( 'express' );
+const dbConection = require('./config/mongo.config.js');
+const conexionDB = require('./config/mongo.config.js');
 const app = express();
+
+
+// Ejecuta la configuracion de Mongoose para Mongo
+//dbConection();      
+conexionDB();       
 
 
 /** Endpoints del sitio */
