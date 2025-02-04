db7df9bf99e8c1513c94190a184eaa9f368d98dd
Juan Carlos Jiménez Gutiérrez <jangojes.dev@gmail.com>
2025-02-03 19:52:56 -0500

    [+] product.controller: Implementar un manejador de exceptiones (try-catch) al registrar el producto

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -10,13 +10,24 @@ function getProducts( req, res ) {
 async function createProduct( req, res ) {
     const inputData = req.body;
 
-    /** Consulta a la BD (Responsabilidad del Servicio) */
-    const data = await ProductModel.create( inputData );  // Mongoose registra en MongoDB
+    try {
+        /** Consulta a la BD (Responsabilidad del Servicio) */
+        const data = await ProductModel.create( inputData );  // Mongoose registra en MongoDB
 
-    res.json({
-        ok: true,
-        data: data
-    });
+        res.json({
+            ok: true,
+            data: data
+        });    
+    } 
+    catch ( error ) {
+        console.error( error );       // Imprime error al Desarrollador
+        // Envia un mensaje de error legible al cliente
+        res.json({                  
+            ok: false,
+            msg: 'Ha ocurrido una excepcion al registrar los datos'
+        });
+    }
+
 }
 
 function getProductById( req, res ) {
