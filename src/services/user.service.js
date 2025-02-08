5b85f0b552bf72321a2e1d37e4b3e3ec4708b927
Juan Carlos Jiménez Gutiérrez <jangojes.dev@gmail.com>
2025-02-07 20:29:43 -0500

    [+] user.service.js: Oculta propiedades (password, createdAt, updatedAt) de la entidad 'Users'

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -13,29 +13,42 @@ async function dbGetUsers() {
 }
 
 async function dbGetUserById( id ) {
-    return await UserModel.findById( id, { password: 0, createdAt: 0, updatedAt: 0, _id: 0 } );           // Forma 1: Usando findById
-    return await UserModel.findOne({ _id: id }, { password: 0, createdAt: 0, updatedAt: 0, _id: 0 });     // Forma 2: Usando findOne
+    // Forma 1: Usando findById
+    return await UserModel.findById( 
+        id, 
+        { password: 0, createdAt: 0, updatedAt: 0, _id: 0 } 
+    );           
+    // Forma 2: Usando findOne
+    return await UserModel.findOne( { _id: id } ).select('-password -createdAt -updatedAt -_id');
 }
 
 async function dbDeleteUserById( id ) {
-    return await UserModel.findByIdAndDelete( id );           // Forma 1: Usando findByIdAndDelete
-    return await UserModel.findOneAndDelete({ _id: id });     // Forma 2: Usando findOneAndDelete
+    // Forma 1: Usando findByIdAndDelete
+    return await UserModel.findByIdAndDelete( 
+        id,
+        { password: 0 }
+    );           
+    // Forma 2: Usando findOneAndDelete
+    return await UserModel.findOneAndDelete(
+        { _id: id },
+        { password: 0 }
+    );     
 }
 
 async function dbUpdateUserById( id, newUser ) {
     // Forma 1: Usando findByIdAndUpdate
     return await UserModel.findByIdAndUpdate( 
         id,                 // id del documento que se va a actualizar
-        newUser,         // Objeto con los datos a actualizar del documento
-        { new: true }       // Retornar los nuevos cambios realizados
+        newUser,            // Objeto con los datos a actualizar del documento
+        { new: true, select: '-password' }      // Retornar los nuevos cambios realizados  
     );
     
     // Forma 2: Usando findOneAndUpdate
     return await UserModel.findOneAndUpdate(
         { _id: id },        // Objeto de consulta con el ID del documento que se va a actualizar
         newUser,         // Objeto con los datos a actualizar del documento
-        { new: true }       // Retornar los nuevos cambios realizados
-    );
+        { new: true}       // Retornar los nuevos cambios realizados
+    ).select( '-password' );
 }
 
 
