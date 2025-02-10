6ba540a8a5d7b843efdc247d295d4a3fd8496c03
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2025-02-10 15:41:54 -0500

    [+] validate-category-exists.middleware.js: Valida si el documento consultado por ID existe [+] category.routes.js: Implementa Middleware en las rutas que solicitan el ID

--- src/routes/category.routes.js
+++ src/routes/category.routes.js
@@ -2,6 +2,7 @@ const express = require( 'express' );
 
 const { createCategory, getCategories, getCategoryById, deleteCategoryById, updateCategoryById } = require('../controllers/category.controller');
 const validateId = require('../middlewares/validate-id.middleware');
+const validateCategoryExists = require('../middlewares/validate-category-exists.middleware');
 
 const router = express.Router();
 
@@ -14,13 +15,13 @@ router.post( '/', createCategory );
 
 // http://localhost:<port>/api/categories/<category-id>
 // req.params.pedro = 7654ftgyhuji
-router.get( '/:id', validateId, getCategoryById );
+router.get( '/:id', [ validateId, validateCategoryExists ], getCategoryById );
 
 // http://localhost:<port>/api/categories/<category-id>
-router.delete( '/:id', validateId, deleteCategoryById );
+router.delete( '/:id', [ validateId, validateCategoryExists ], deleteCategoryById );
 
 // http://localhost:<port>/api/categories/<category-id>
-router.patch( '/:id', validateId, updateCategoryById );
+router.patch( '/:id', [ validateId, validateCategoryExists ], updateCategoryById );
 
 
 module.exports = router;
